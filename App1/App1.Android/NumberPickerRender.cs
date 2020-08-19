4cd59646f3dec7eae1a70a86386d60ed30edba68
武扬 李 <liwuyang1234@qq.com>
2020-08-19 19:31:36 +0800

    aa

--- App1/App1.Android/NumberPickerRender.cs
+++ App1/App1.Android/NumberPickerRender.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using App1.Droid;
 using SuperRabbit.Lib;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Platform.Android;
 
 [assembly: Xamarin.Forms.ExportRenderer(typeof(App1.NumberPicker2), typeof(NumberPickerRender))]
@@ -20,6 +23,7 @@ namespace App1.Droid
     {
         public NumberPickerRender(Context context) : base(context)
         {
+
         }
 
 
@@ -49,13 +53,33 @@ namespace App1.Droid
         protected override void OnAttachedToWindow()
         {
             base.OnAttachedToWindow();
+
             wheelPicker.SetWheelItemCount(3);
             wheelPicker.SetAdapter(new DaysAdapter());
             wheelPicker.SetMin(0);
             wheelPicker.SetSelectorRoundedWrapPreferred(true);
+            wheelPicker.ValueChange += WheelPicker_ValueChange;
+            wheelPicker.SetSelectedTextColor(Resource.Color.custmonColor);
+            //开始反射
+            var fss = wheelPicker.Class.GetDeclaredFields();
+            var _class = wheelPicker.Class;
+            var des = Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Density;
+            var sizeField = _class.GetDeclaredField("mTextSize");
+            sizeField.Accessible = true;
+            sizeField.Set(wheelPicker, (int)Math.Ceiling(Element.FontSize * des));
+            var selecedColorField = _class.GetDeclaredField("mSelectedTextColor");
+            selecedColorField.Accessible = true;
+            selecedColorField.Set(wheelPicker, (int)Element.SelectedColor.ToAndroid());
+            var unselecedColorField = _class.GetDeclaredField("mUnSelectedTextColor");
+            unselecedColorField.Accessible = true;
+            unselecedColorField.Set(wheelPicker, (int)Element.UnSelectedTextColor.ToAndroid());
         }
 
+        private void WheelPicker_ValueChange(object sender, ValueChangeEventArgs e)
+        {
+            Element.SelectedIndex = int.Parse(e.NewVal) - 1;
 
+        }
     }
 
     class DaysAdapter : Java.Lang.Object, IWheelAdapter
@@ -68,12 +92,12 @@ namespace App1.Droid
 
         public int GetPosition(string value)
         {
-            return int.Parse(value)-1;
+            return int.Parse(value) - 1;
         }
 
         public string GetValue(int position)
         {
-            if (position==-2)
+            if (position == -2)
             {
                 return "30";
             }
@@ -81,7 +105,7 @@ namespace App1.Droid
             {
                 return "31";
             }
-            return (position+1).ToString();
+            return (position + 1).ToString();
         }
     }
 }
--- App1/App1/NumberPicker2.cs
+++ App1/App1/NumberPicker2.cs
@@ -9,6 +9,12 @@ namespace App1
 {
     public class NumberPicker2 : View
     {
+        public int SelectedIndex { get; set; }
 
+        public Color SelectedColor { get; set; } = Color.Red;
+
+        public Color UnSelectedTextColor { get; set; } = Color.Blue;
+
+        public double FontSize { get; set; } = 40;
     }
 }
